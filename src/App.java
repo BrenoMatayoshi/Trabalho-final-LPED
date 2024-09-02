f5fd2ff6ac8701390ae9a814b7839a7dbcba0f9b
Breno Masato Matayoshi <145488931+BrenoMatayoshi@users.noreply.github.com>
2024-09-02 10:33:01 -0300

    Toda a parte de usuário concluída
    Métodos criados:
    carregarDadosUsuario(); parâmetros: caminho do arquivo, ArrayList tipo String; função: alimenta um ArrayList com os dados de um arquivo.
    escreverDadosUsuario(); parâmetros: caminho do arquivo, ArraList tipo String; função: escreve em um arquivo os dados de um ArrayList.
    verificarArrayListUsuario(); parâmetros: elemento, ArrayList tipo String; função: verifica se um elemento existe em um ArrayList.
    printarUsuario(); parâmetros: ArrayList tipo String; função: printa todos os usuários.
    escolherUsuario(); parâmetros: scanner; função: escolhe, cria e remove usuarios.

--- src/App.java
+++ src/App.java
@@ -1,25 +1,149 @@
-//import java.io.BufferedReader;
-//import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
+    private static final String caminhoUsuario = "usuario.txt";
+
+    // Método para carregar os dados de um arquivo em um ArrayList
+    public static void carregarDadosUsuario(String caminho, ArrayList<String> arraylist) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(caminho));
+            String adicionarAoArrayList = br.readLine();
+            while (true) {
+                if (adicionarAoArrayList != null) {
+                    arraylist.add(adicionarAoArrayList);
+                } else {
+                    br.close();
+                    break;
+                }
+                adicionarAoArrayList = br.readLine();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    // Método para escrever os dados em um arquivo.
+    // caminho = caminho do arquivo
+    // arrayList = lista que voce quer escrever
+    public static void escreverDadosUsuario(String caminho, ArrayList<String> arrayList) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(caminho));
+            for (Object object : arrayList) {
+                bw.append(object + "\n");
+            }
+            bw.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    // Método para verificar se um elemento existe dentro do arrayList
+    // Se o retorno for -1 o elemento não existe dentro do arrayList
+    public static int verificarArrayListUsuario(String dado, ArrayList<String> arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i).equalsIgnoreCase(dado)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Printa todo o arrayList
+    public static void printarUsuario(ArrayList<String> arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println((i + 1) + " - " + arrayList.get(i));
+        }
+    }
+
+    // Método para selecionar/cadastrar/remover usuario
     public static String escolherUsuario(Scanner scanner) {
-        String usuario = null;
-        int escolha;
+        String usuario = null, receberNome;
+        int escolha = -1, escolhaUsuario;
+        ArrayList<String> listaUsuario = new ArrayList<>();
+        carregarDadosUsuario(caminhoUsuario, listaUsuario);
         while (usuario == null) {
             try {
-                System.out.println("1 - Escolher usuário.\n2 - Cadastrar usuário.");
+                System.out.println("1 - Escolher usuário.\n2 - Cadastrar usuário.\n3 - Remover usuário.\n0 - Sair.");
                 escolha = scanner.nextInt();
                 scanner.nextLine();
                 switch (escolha) {
                     case 1:
-                        // A fazer
+                        if (!listaUsuario.isEmpty()) {
+                            System.out.println("Escolha um usuário:");
+                            while (true) {
+                                System.out.println("0 - Voltar");
+                                printarUsuario(listaUsuario);
+                                try {
+                                    escolhaUsuario = scanner.nextInt();
+                                    scanner.nextLine();
+                                    if (escolhaUsuario == 0) {
+                                        break;
+                                    } else {
+                                        if (escolhaUsuario < 1 || escolhaUsuario > listaUsuario.size()) {
+                                            System.out.println("Usuário não encontrado.");
+                                        } else {
+                                            usuario = listaUsuario.get(escolhaUsuario - 1);
+                                            limparTela();
+                                            System.out.println("\nOlá " + usuario + ", seja bem-vindo!\n");
+                                            return usuario;
+                                        }
+                                    }
+                                } catch (Exception e) {
+                                    scanner.nextLine();
+                                    System.out.println("Usuário não encontrada.");
+                                }
+                            }
+                        } else {
+                            System.out.println("\nNenhum usuário cadastrado.\n");
+                        }
                         break;
 
                     case 2:
-                        // A fazer
+                        System.out.println("Informe o seu nome: ");
+                        receberNome = scanner.nextLine();
+                        if (verificarArrayListUsuario(receberNome, listaUsuario) == -1) {
+                            listaUsuario.add(receberNome);
+                            escreverDadosUsuario(caminhoUsuario, listaUsuario);
+                            System.out.println("\nUsuário adicionado com sucesso.\n");
+                        } else {
+                            System.out.println("Esse usuário já existe.");
+                        }
                         break;
+
+                    case 3:
+                        if (!listaUsuario.isEmpty()) {
+                            try {
+                                System.out.println("Selecione o usuário que voce deseja remover: \n0 - Voltar");
+                                printarUsuario(listaUsuario);
+                                escolhaUsuario = scanner.nextInt();
+                                if (escolhaUsuario == 0) {
+                                    break;
+                                }
+                                if (escolhaUsuario < 0 || escolhaUsuario > listaUsuario.size()) {
+                                    System.out.println("Usuário não existe.");
+                                } else {
+                                    listaUsuario.remove(escolhaUsuario - 1);
+                                    escreverDadosUsuario(caminhoUsuario, listaUsuario);
+                                    System.out.println("\nUsuário removido com sucesso.\n");
+                                }
+                            } catch (Exception e) {
+                                System.out.println("Opção não encontrada.");
+                                scanner.nextLine();
+                                escolhaUsuario = -1;
+                            }
+                        } else {
+                            System.out.println("\nNenhum usuário cadastrado.\n");
+                        }
+                        break;
+
+                    case 0:
+                        return ".";
 
                     default:
                         System.out.println("Opção não encontrada.");
@@ -34,11 +158,26 @@ public class App {
         return usuario;
     }
 
+    // Limpa a tela
+    public static void limparTela() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         int escolha = -1;
         Scanner scanner = new Scanner(System.in);
         String usuario = escolherUsuario(scanner);
-        System.out.println("Olá " + usuario + " seja bem vindo.");
+        if (usuario.equals(".")) {
+            escolha = 0;
+        }
         while (escolha != 0) {
             try {
                 System.out.println("O que você deseja fazer?\n1 - Add\n2 - Remover\n3 - Trocar\n4 - Listar\n5 - Procurar\n6 - Exibir historico\n0 - Encerrar Programa.");
@@ -46,29 +185,33 @@ public class App {
                 scanner.nextLine();
                 switch (escolha) {
                     case 1:
-                        
+
                         break;
-                
+
                     case 2:
-                        
+
                         break;
-                
+
                     case 3:
-                        
+
                         break;
-                
+
                     case 4:
-                        
+
                         break;
-                
+
                     case 5:
-                        
+
                         break;
-                
+
                     case 6:
-                        
+
                         break;
-                
+
+                    case 0:
+                        System.out.println("Programa encerrado com sucesso.");
+                        break;
+
                     default:
                         System.out.println("Opção não encontrada.");
                         break;
@@ -79,5 +222,6 @@ public class App {
                 escolha = -1;
             }
         }
+        scanner.close();
     }
 }
